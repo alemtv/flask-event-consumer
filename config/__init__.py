75d944abdbc86f434411d9cd18ef0b1313a97df4
Aleksandr <turiukompa@gmail.com>
2023-04-03 20:05:58 +0300

    update config ini

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,10 @@
 import pathlib
 import tomli
-import os
 
 path = pathlib.Path(__file__).parent / "event-consumer.toml"
 with path.open(mode="rb") as fp:
     event_consumer_config = tomli.load(fp)
-    PAYLOADS_FILE = os.path.join(event_consumer_config['payloads_folder'],
-                                 event_consumer_config['payloads_file'])
+    PAYLOADS_FILE = (pathlib.Path(
+        event_consumer_config['payloads_folder']) /
+        event_consumer_config['payloads_file']
+        )
